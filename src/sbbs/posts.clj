6822f071a9cb704f523f5d9c9fad3acda031edb9
Kyle Isom <kyle@tyrfingr.is>
2012-06-13 19:06:37 +0300

    Adding records to represent comments, categories, and users.

---
+++ src/sbbs/posts.clj
@@ -0,0 +1,32 @@
+(ns sbbs.posts)
+
+;;; a comment consists of the following fields:
+;;;    id
+;;;    userid          the userid of the user posting the comment
+;;;    posted_at       the date and time the comment was first posted
+;;;    edited_at       the date and time the comment was last edited
+;;;                    (or 0 if not edited)
+;;;    text            the body of the comment
+;;;    parent          the id of the first comment in the thread; zero
+;;;                    if the comment is the first comment in the thread
+;;;    category        which category does the comment belong to;
+;;;                    primarily useful for the parent comment
+(defrecord Comment
+    #^{ :doc "representation of a comment in a thread" }
+  [id userid posted_at edited_at text parent category])
+
+;;; a User is tied to the underlying UNIX host; ergo, all permissions
+;;; and user names are those assigned by the system
+;;; administrator. Consequently, all access control is done via UNIX
+;;; groups. This record exists solely to tie a user's login name to
+;;; the database.
+(defrecord User
+    #^{ :doc "database representation of a user, tied to UNIX logname"}
+  [id name])
+
+;;; A category (sometimes called a subforum) is simply an
+;;; organisational label to logically group a collection of threads.
+(defrecord Category
+    #^{ :doc "Representation of a category or subforum, a collection of
+threads" }
+  [id name description])
