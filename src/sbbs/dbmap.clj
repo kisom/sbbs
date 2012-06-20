bcc7196540a38f9c7a04651672cef794ea985fc8
Kyle Isom <kyle@tyrfingr.is>
2012-06-21 00:33:20 +0300

    clean up database load

--- src/sbbs/dbmap.clj
+++ src/sbbs/dbmap.clj
@@ -5,15 +5,17 @@
   (:import [sbbs.records User])
   (:import [sbbs.records Category]))
 
-;;; connect to the category database
+;;; connect to the component databases
 ;;;     TODO: read from env
+
+;;; right now, I don't know how to read env vars in clojure, so
+;;; dbnames are hardcoded
 (def -sbbs-categorydb-name "sbbs_dev1_categories")
-(def sbbs-categorydb (get-database -sbbs-categorydb-name))
-
 (def -sbbs-commentdb-name "sbbs_dev1_comments")
-(def sbbs-commentdb (get-database -sbbs-commentdb-name))
-
 (def -sbbs-userdb-name "sbbs_dev1_users")
+
+(def sbbs-categorydb (get-database -sbbs-categorydb-name))
+(def sbbs-commentdb (get-database -sbbs-commentdb-name))
 (def sbbs-userdb (get-database -sbbs-userdb-name))
 
 ;;; currently a stubbed pass-through, enables us to easily support
